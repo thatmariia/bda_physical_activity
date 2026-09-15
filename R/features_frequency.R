258bc94ba262edc6615c5195f3f7d412b7b2cae1
Mariia Steeghs-Turchina <mariia.turchina@gmail.com>
2026-09-15 13:33:07 +0200

    feat: freq init

--- R/features_frequency.R
+++ R/features_frequency.R
@@ -1,5 +1,9 @@
 # Feature functions (frequency domain)
 
+#' Compute the mean frequency of a signal given its frequency and spectrum
+#' @param freq A numeric vector of frequencies corresponding to the spectrum
+#' @param spec A numeric vector representing the spectrum (power spectral density) of the signal
+#' @return The mean frequency of the signal
 mean_frequency <- function(freq, spec) {
   delta_f <- freq[2] - freq[1] # gap size between frequencies
   normalizing_constant <- sum(spec * delta_f) # ≈ ∫S(f)df
@@ -7,27 +11,24 @@ mean_frequency <- function(freq, spec) {
   return(mean_freq)
 }
 
-#' Convert a signal data frame into a segmented frequency domain representation
-#' @param signal_df A data frame containing the signal data
-#' @param n_samples_per_epoch The number of samples per epoch (default is 128, corresponding to 2.56 seconds at 50 Hz)
-#' @param sample_rate The sample rate of the signal in Hz (default is 50 Hz)
-#' @return A data frame containing the frequency domain representation for each epoch,
-#' including the frequency bins and corresponding spectral densities for each signal.
-convert_signal_to_spectrum <- function(signal_df, n_samples_per_epoch = 128, sample_rate = 50) {
-  spectrum_df <- signal_df |>
-    mutate(epoch = sampleid %/% n_samples_per_epoch) |>
-    reframe(
-      {
-        sp <- spectrum(cbind(X1, X2, X3), span = 15, plot = FALSE)
-        tibble(
-          freq = sp$freq, # cycles per sample
-          freq_hz = sp$freq * sample_rate, # cycles per second (Hz)
-          spec1 = sp$spec[, 1],
-          spec2 = sp$spec[, 2],
-          spec3 = sp$spec[, 3]
-        )
-      },
-      .by = epoch
-    )
-  return(spectrum_df)
+#' Extract frequency domain features from a spectrum data frame,
+#' which is assumed to be already segmented into epochs
+#' @param spectrum_df A data frame containing the spectrum data with columns: epoch, freq, spec1, spec2, ...
+#' @return A data frame containing the extracted frequency domain features for each epoch
+get_frequency_domain_features <- function(spectrum_df) {
+  userfreqdom <- spectrum_df %>%
+    group_by(epoch) %>%
+    summarise(
+      # Dominant frequency of signal 1
+      domfreq1 = freq[which.max(spec1)],
+
+      # Mean frequency of signal 2
+      meanfreq2 = mean_frequency(freq, spec2),
+
+      # Example of other possible features
+      # (you can define your own functions like before)
+      # spectral_entropy = ...
+      # bandpower_low   = ...
+      # bandpower_high  = ...
+  )
 }
